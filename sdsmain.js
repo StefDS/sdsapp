157a22b285a8cbee5e76fe60f7ca1e77630d4372
StefDS <stefan.deschuyter@gmail.com>
2021-04-22 14:52:01 +0200

    Convert code to support JSON

---
+++ sdsmain.js
@@ -0,0 +1,44 @@
+///
+/// Stef's NodeJS Webserver for Docker & Kubernetes experiments
+///
+var myip = require('ip');
+var myos = require('os');
+var mycounter = require("./sdsmodules.js");
+var http = require('http');
+const hostname = '0.0.0.0';
+const port = 8088;
+var vhost = myos.hostname;
+var vcounter =  mycounter.getcounter(vhost);
+
+//const args = process.argv.slice(2);
+
+const server = http.createServer((req, res) => {
+  /// 
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.write("<b>Sample Web App</b><br>");
+  res.write("<img src='logo.png' alt='HPE logo'><br>");
+  res.write("<b>** New Tech In Action - Summer 2021 **</b><br><br>");
+  res.write("      Node Name: " + vhost+  "<br>");
+  res.write("   Node Address: " + myip.address() + " port: "+ server.address().port + "<br>");
+  res.write("        Counter: " + vcounter + "<br>");
+  res.write("    Time is now: " + Date().toString()+ "<br>");
+  var urldata = req.url;   
+  if (urldata == "/exit") { 
+    res.write("*** webserver stopped ***<br>");
+    console.log("*** webserver stopped ***");
+    res.end(); 
+    process.exit();
+  }
+  res.write("URL data: " + urldata);
+  res.end();
+  vcounter =  mycounter.getcounter(vhost); 
+});
+
+server.listen(port, hostname, () => {
+  console.log("Server listing on port " + port.toString() );
+});
+
+var callback = function (err, data) {
+  if (err) return console.error(err);
+  console.log(data);
+};
---
+++ sdsmodules.js
@@ -0,0 +1,35 @@
+exports.getcounter = function (thisHostname) {
+    var fs = require('fs');
+    const data = fs.readFileSync('./data/app.json', 'utf8');
+    console.log("Hostname= ", thisHostname);
+    //
+    var JSONdata = JSON.parse(data);
+    /// reading File
+    var thisCounter = JSONdata.counter;
+    console.log("JSONcounter=", JSONdata.counter );
+    console.log("JSONhostname=", JSONdata.hostname );
+    if (isNaN(thisCounter)) {
+        console.log("** Problem reading data **");
+        return 0;
+    };
+    thisCounter += 1;
+    ///
+    ///
+    JSONdata.counter = thisCounter;
+    JSONdata.hostname = thisHostname;
+    console.log(JSON.stringify(JSONdata, null, 4))
+    //
+    const thisWrite = '{"counter": ' + thisCounter + ', "hostname": "' + thisHostname + '"}';
+    // const thisWrite = JSON.stringify({"counter": thisCounter, "hostname": thisHostname, "value": "test"}, null, 2);
+    // var thisWrite = JSON.stringify(JSONdata, null, 4);
+    console.log("Write= " + thisWrite);
+    //    fswrite.writeFilesync('./app.log', thiswrite); 
+    fs.writeFile("./data/app.json", thisWrite, function (err) {
+        if (err) {
+            console.log(err);
+            console.log("** Problem writing data **");
+            return 0;
+        }
+    });
+    return thisCounter;
+};
